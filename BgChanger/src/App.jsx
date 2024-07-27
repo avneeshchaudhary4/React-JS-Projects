027971baeed6f61567b2e984b9967408bffb6ceb
Avneesh Patel <139247333+avneeshpatel4@users.noreply.github.com>
2024-07-27 14:03:51 +0530

    Update App.jsx

--- BgChanger/src/App.jsx
+++ BgChanger/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       justify-center  bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button onClick={()=> setColor("red")}
-          className="outline-none px-4 py-1
+          className="outline-none px-4 py-2
           rounded-full text-white shadow-lg"
           style = {{backgroundColor: "red"}}>Red</button>
            <button onClick={()=> setColor("green")}
